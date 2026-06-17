6b7106fc426e26fdb2bfb4ea59671f0afffa166f
Suraj Kumar Mishra <surajmishra7654@gmail.com>
2026-06-17 14:57:04 +0530

    Completed and tested Section 14 stored procedure

--- database/sp_submit_section14_application.sql
+++ database/sp_submit_section14_application.sql
@@ -10,8 +10,10 @@ AS
 $$
 DECLARE
     v_caseid VARCHAR;
-    v_petitioner JSONB;
-    v_land JSONB;
+    v_officename VARCHAR;
+    v_officeplace VARCHAR;
+    v_petitioner JSON;
+    v_land JSON;
 BEGIN
 
     -- Validate Client
@@ -26,51 +28,136 @@ BEGIN
         p_case_data->>'caseyear'
     );
 
+    SELECT
+    officename,
+    officeplace
+    INTO
+        v_officename,
+        v_officeplace
+    FROM masdistrict
+    WHERE statecode = p_case_data->>'statecode'
+    AND distcode = p_case_data->>'distcode'
+    LIMIT 1;
+
     -- Insert Case Details
     INSERT INTO tblrevcases
     (
         caseid,
         statecode,
+        officeid,
+        officename,
+        officeplace,
         distcode,
+        subdivcode,
+        revcirclecode,
         caseyear,
         subject,
-        undersection
+        undersection,
+        ifcsc
     )
     VALUES
     (
         v_caseid,
         p_case_data->>'statecode',
+        p_case_data->>'officeid',
+        v_officename,
+        v_officeplace,
         p_case_data->>'distcode',
+        p_case_data->>'subdivcode',
+        p_case_data->>'revcirclecode',
         p_case_data->>'caseyear',
         p_case_data->>'subject',
-        p_case_data->>'undersection'
+        p_case_data->>'undersection',
+        FALSE
     );
+
 
     -----------------------------------------------------------------
     -- Insert Petitioners
     -----------------------------------------------------------------
 
-    -- FOR v_petitioner IN
-    --     SELECT * FROM jsonb_array_elements(p_case_data->'petitioners')
-    -- LOOP
-    --     INSERT INTO tblpetitioner (...);
-    -- END LOOP;
+    FOR v_petitioner IN
+        SELECT *
+        FROM jsonb_array_elements(p_case_data->'petitioners')
+    LOOP
+
+        INSERT INTO tblpetitioner
+        (
+            caseid,
+            petisl,
+            petiname,
+            petirelation,
+            petifh_name,
+            petiaddress,
+            peticontactno,
+            petiemail
+        )
+        VALUES
+        (
+            v_caseid,
+            (v_petitioner->>'petisl')::INTEGER,
+            v_petitioner->>'petiname',
+            v_petitioner->>'petirelation',
+            v_petitioner->>'petifh_name',
+            v_petitioner->>'petiaddress',
+            v_petitioner->>'peticontactno',
+            v_petitioner->>'petiemail'
+        );
+
+    END LOOP;
 
     -----------------------------------------------------------------
     -- Insert Land Details
     -----------------------------------------------------------------
 
-    -- FOR v_land IN
-    --     SELECT * FROM jsonb_array_elements(p_case_data->'lands')
-    -- LOOP
-    --     INSERT INTO tbllanddetail (...);
-    -- END LOOP;
+    FOR v_land IN
+        SELECT *
+        FROM jsonb_array_elements(p_case_data->'lands')
+    LOOP
+
+        INSERT INTO tbllanddetail
+        (
+            caseid,
+            moucode,
+            ktsr,
+            ptsr,
+            area,
+            main_class_code,
+            sub_class_code
+        )
+        VALUES
+        (
+            v_caseid,
+            v_land->>'moucode',
+            v_land->>'ktsr',
+            v_land->>'ptsr',
+            (v_land->>'area'),
+            v_land->>'main_class_code',
+            v_land->>'sub_class_code'
+        );
+
+    END LOOP;
 
     -----------------------------------------------------------------
     -- Insert User Log
     -----------------------------------------------------------------
 
-    -- INSERT INTO tbluserlog (...);
+    INSERT INTO tbluserlog
+    (
+        caseid,
+        taskinfo,
+        userid,
+        dttask,
+        entcomputer
+    )
+    VALUES
+    (
+        v_caseid,
+        'Section 14 Application Submitted',
+        p_case_data->'user'->>'userid',
+        NOW(),
+        p_case_data->'user'->>'computer'
+    );
 
     o_caseid := v_caseid;
 
